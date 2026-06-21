8a0a3db88c1f151659c51b224be7acb96d453bf3
Chris Eddy <chris.eddy.email@gmail.com>
2026-06-20 18:27:41 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeepSpacePlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
